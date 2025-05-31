1624a1ba175f8f9183b9a70eb1c483e670f46b74
Sitati Patience <patiencesitati99@gmail.com>
2025-05-31 18:27:48 +0300

    Update settings.py

--- contact/contact/settings.py
+++ contact/contact/settings.py
@@ -40,14 +40,13 @@ INSTALLED_APPS = [
     'contacts',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #for testing in terminal
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', 'webmaster@localhost')
 
 
 MIDDLEWARE = [
